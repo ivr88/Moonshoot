df6f6dd8f444bc111a081da0d172e116aea2cd40
ivr88 <114075652+ivr88@users.noreply.github.com>
2023-11-30 19:14:33 +0300

    Initial Commit

---
+++ Moonshoot/MoonshootApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoonshootApp.swift
+//  Moonshoot
+//
+//  Created by Вадим Исламов on 30.11.2023.
+//
+
+import SwiftUI
+
+@main
+struct MoonshootApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
